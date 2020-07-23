124ce6edadb04439384bf66c741c25493ec14441
Thomas Hibbard <thibbard@overnitecbt.com>
2020-07-23 14:44:56 -0500

    Forcing in the file extension.

--- import_export/admin.py
+++ import_export/admin.py
@@ -551,21 +551,21 @@ class ExportActionMixin(ExportMixin):
         filename = request.POST.get('filename')
 
         if int(export_format) == 0:
-            filename += ".csv"
+            filename = filename + ".csv"
         elif int(export_format) == 1:
-            filename += ".xls"
+            filename = filename + ".xls"
         elif int(export_format) == 2:
-            filename += ".xlsx"
+            filename = filename + ".xlsx"
         elif int(export_format) == 3:
-            filename += ".tsv"
+            filename = filename + ".tsv"
         elif int(export_format) == 4:
-            filename += ".ods"
+            filename = filename + ".ods"
         elif int(export_format) == 5:
-            filename += ".json"
+            filename = filename +  ".json"
         elif int(export_format) == 6:
-            filename += ".yaml"
+            filename = filename + ".yaml"
         else:
-            filename += ".html"
+            filename = filename +  ".html"
 
         if not export_format:
             messages.warning(request, _('You must select an export format and define a filename.'))
